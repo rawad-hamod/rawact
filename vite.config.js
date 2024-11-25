5820235c68e3c6ff13d15eb1c0ae80d7191df61f
rawad-hamod <rawadwhamod@gamil.com>
2024-11-25 08:51:17 +0300

    editing vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+export default {
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base:'/rawact/'
-})
+  "base":"/rawact/"
+  
+  }
